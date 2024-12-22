fc034623528df4012cbf10e32ff77937de65eea5
kimi_no_shiriai <94500383+C4se-K@users.noreply.github.com>
2024-12-22 05:29:19 -0800

    Update poc_2.py

--- poc_2.py
+++ poc_2.py
@@ -1,11 +1,38 @@
 from faster_whisper import WhisperModel
 import os
 import pathlib
-import time  # Import the time module
+import time
 
 model_size = "large-v3"
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+"""
+output: 
+
+beam 1 Average Time taken for transcription: 0.97 seconds. time per second: 0.03 with range of: 0.0681        
+beam 2 Average Time taken for transcription: 1.04 seconds. time per second: 0.03 with range of: 0.0307        
+beam 3 Average Time taken for transcription: 1.11 seconds. time per second: 0.03 with range of: 0.0921
+beam 4 Average Time taken for transcription: 1.76 seconds. time per second: 0.05 with range of: 0.0850
+beam 5 Average Time taken for transcription: 1.82 seconds. time per second: 0.05 with range of: 0.0526
+beam 6 Average Time taken for transcription: 1.97 seconds. time per second: 0.06 with range of: 0.1039
+beam 7 Average Time taken for transcription: 2.05 seconds. time per second: 0.06 with range of: 0.1422
+beam 8 Average Time taken for transcription: 1.95 seconds. time per second: 0.06 with range of: 0.0751
+beam 9 Average Time taken for transcription: 2.25 seconds. time per second: 0.07 with range of: 0.0908
+beam 10 Average Time taken for transcription: 2.51 seconds. time per second: 0.07 with range of: 0.2888
+beam 11 Average Time taken for transcription: 2.53 seconds. time per second: 0.07 with range of: 0.2479
+beam 12 Average Time taken for transcription: 2.64 seconds. time per second: 0.08 with range of: 0.0991
+beam 13 Average Time taken for transcription: 2.80 seconds. time per second: 0.08 with range of: 0.1175
+beam 14 Average Time taken for transcription: 3.01 seconds. time per second: 0.09 with range of: 0.2720
+beam 15 Average Time taken for transcription: 3.24 seconds. time per second: 0.10 with range of: 0.1895
+beam 16 Average Time taken for transcription: 2.94 seconds. time per second: 0.09 with range of: 0.1385
+beam 17 Average Time taken for transcription: 3.49 seconds. time per second: 0.10 with range of: 0.2092
+beam 18 Average Time taken for transcription: 3.74 seconds. time per second: 0.11 with range of: 0.1914
+beam 19 Average Time taken for transcription: 3.84 seconds. time per second: 0.11 with range of: 0.1641
+beam 20 Average Time taken for transcription: 3.98 seconds. time per second: 0.12 with range of: 0.0758
+
+
+"""
 
 # Run on GPU with FP16
 model = WhisperModel(model_size, device="cuda", compute_type="float16")
