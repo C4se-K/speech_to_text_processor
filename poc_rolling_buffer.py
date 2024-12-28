925bf0fe67e61e11fb41580b9953335628bc1686
kimi_no_shiriai <94500383+C4se-K@users.noreply.github.com>
2024-12-28 04:27:06 -0800

    Update poc_rolling_buffer.py

--- poc_rolling_buffer.py
+++ poc_rolling_buffer.py
@@ -9,15 +9,16 @@ various tests while building a rolling buffer
 
 """
 
-
-CHUNK = 1024  #samples per frame
+RATE = 16000  # sampling rate
+CHUNK = int(RATE * 0.01)  #samples per frame -> 10 ms chunks, 16000 samples per frame
 FORMAT = pyaudio.paInt16  # 16-bit resolution
 CHANNELS = 1  # mono
-RATE = 44100  # sampling rate
+BUFFER_DURATION = 1 # length in time for the buffer
+BUFFER_SIZE = RATE * BUFFER_DURATION
 
 p = pyaudio.PyAudio()
 
-BUFFER_SIZE = 10 * CHUNK
+
 
 # open stream
 stream = p.open(format=FORMAT,
@@ -29,16 +30,28 @@ stream = p.open(format=FORMAT,
 rolling_buffer = deque(maxlen = BUFFER_SIZE)
 print("buffer start...")
 
+output_stream = p.open(format=pyaudio.paInt16,
+                       channels=1,
+                       rate=RATE,
+                       output=True)
+
 try:
     while True:
         data = stream.read(CHUNK, exception_on_overflow=False)
-
         audio_data = np.frombuffer(data, dtype=np.int16)
-
         rolling_buffer.extend(audio_data)
 
-        #print(audio_data) # test 1 -> confirm that audio data is recieved
-        print(f"Buffer length: {len(rolling_buffer)}") # test 2 -> confirm that the buffer is being populated
+        # test 1 -> confirm that audio data is recieved
+        #print(audio_data) 
+
+        # test 2 -> confirm that the buffer is being populated
+        #print(f"Buffer length: {len(rolling_buffer)}") 
+
+        # test 3 -> confirm that the audio being saved to the buffer is indeed audible data
+        #output_stream.write(data) 
+
+
+
 
 except KeyboardInterrupt:
     print("\nRecording stopped.")
